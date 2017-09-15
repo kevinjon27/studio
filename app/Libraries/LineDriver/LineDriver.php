87a59eeafab41a054db77c32288c8ba9d405426b
Kevin Jonathan <kevin_jon27@yahoo.com>
2017-09-16 04:57:54 +0700

    update getMessage function for handling events and update getUser function to get user data

--- app/Libraries/LineDriver/LineDriver.php
+++ app/Libraries/LineDriver/LineDriver.php
@@ -5,9 +5,26 @@
 
     namespace App\Libraries\LineDriver;
 
-use BotMan\BotMan\Messages\Incoming\Answer;
-use LINE\LINEBot\Exception\InvalidEventRequestException;
-use LINE\LINEBot\Exception\InvalidSignatureException;
+    use App\Libraries\LineDriver\Extensions\User;
+    use BotMan\BotMan\Messages\Incoming\Answer;
+    use Illuminate\Support\Facades\Log;
+    use LINE\LINEBot\Event\BeaconDetectionEvent;
+    use LINE\LINEBot\Event\FollowEvent;
+    use LINE\LINEBot\Event\JoinEvent;
+    use LINE\LINEBot\Event\LeaveEvent;
+    use LINE\LINEBot\Event\MessageEvent;
+    use LINE\LINEBot\Event\MessageEvent\AudioMessage;
+    use LINE\LINEBot\Event\MessageEvent\ImageMessage;
+    use LINE\LINEBot\Event\MessageEvent\LocationMessage;
+    use LINE\LINEBot\Event\MessageEvent\StickerMessage;
+    use LINE\LINEBot\Event\MessageEvent\TextMessage;
+    use LINE\LINEBot\Event\MessageEvent\UnknownMessage;
+    use LINE\LINEBot\Event\MessageEvent\VideoMessage;
+    use LINE\LINEBot\Event\PostbackEvent;
+    use LINE\LINEBot\Event\UnfollowEvent;
+    use LINE\LINEBot\Event\UnknownEvent;
+    use LINE\LINEBot\Exception\InvalidEventRequestException;
+    use LINE\LINEBot\Exception\InvalidSignatureException;
 
     use BotMan\BotMan\Drivers\HttpDriver;
     use BotMan\BotMan\Interfaces\DriverInterface;
@@ -35,6 +52,8 @@ use LINE\LINEBot\Exception\InvalidSignatureException;
 
         protected $matchesRequest = false;
 
+        protected $log;
+
         /**
          * @param Request $request
          */
@@ -46,14 +65,17 @@ use LINE\LINEBot\Exception\InvalidSignatureException;
             $this->line = new LINEBot(new CurlHTTPClient($this->config->get('channel_access_token')),[
                 'channelSecret' => $this->config->get('channel_secret')
             ]);
+            $this->log = new Log();
 
             try {
                 $this->event = Collection::make($this->line->parseEventRequest($request->getContent(), $this->signature));
                 $this->matchesRequest = true;
             } catch (InvalidSignatureException $e) {
                 $this->matchesRequest = false;
+                $this->log->error('Invalid signature: ', $e->getMessage());
             } catch (InvalidEventRequestException $e) {
                 $this->matchesRequest = false;
+                $this->log->error('Invalid event request:', $e->getMessage());
             }
 
         }
@@ -72,7 +94,56 @@ use LINE\LINEBot\Exception\InvalidSignatureException;
         {
             if (empty($this->messages)) {
                 foreach ($this->event as $event) {
-                    $this->messages[] = new IncomingMessage($event->getText(), $event->getUserId(), $event->getReplyToken());
+                    if ($event instanceof MessageEvent) {
+                        if ($event instanceof TextMessage) {
+                            $this->messages[] = new IncomingMessage($event->getText(), $event->getUserId(), $event->getReplyToken());
+                        } elseif ($event instanceof StickerMessage) {
+
+                        } elseif ($event instanceof LocationMessage) {
+
+                        } elseif ($event instanceof ImageMessage) {
+
+                        } elseif ($event instanceof AudioMessage) {
+
+                        } elseif ($event instanceof VideoMessage) {
+
+                        } elseif ($event instanceof UnknownMessage) {
+                            $this->log->info(sprintf(
+                                              'Unknown message type has come [message type: %s]',
+                                              $event->getMessageType()
+                                          ));
+                        } else {
+                            // Unexpected behavior (just in case)
+                            // something wrong if reach here
+                            $this->log->info(sprintf(
+                                              'Unexpected message type has come, something wrong [class name: %s]',
+                                              get_class($event)
+                                          ));
+                            continue;
+                        }
+                    } elseif ($event instanceof UnfollowEvent) {
+
+                    } elseif ($event instanceof FollowEvent) {
+
+                    } elseif ($event instanceof JoinEvent) {
+
+                    } elseif ($event instanceof LeaveEvent) {
+
+                    } elseif ($event instanceof PostbackEvent) {
+
+                    } elseif ($event instanceof BeaconDetectionEvent) {
+
+                    } elseif ($event instanceof UnknownEvent) {
+                        $this->log->info(sprintf('Unknown message type has come [type: %s]', $event->getType()));
+                    } else {
+                        // Unexpected behavior (just in case)
+                        // something wrong if reach here
+                        $this->log->info(sprintf(
+                                          'Unexpected event type has come, something wrong [class name: %s]',
+                                          get_class($event)
+                                      ));
+                        continue;
+                    }
                 }
             }
             return $this->messages;
@@ -85,7 +156,20 @@ use LINE\LINEBot\Exception\InvalidSignatureException;
 
         public function getUser(IncomingMessage $matchingMessage)
         {
-            // TODO: Implement getUser() method.
+            $user =  $this->line->getProfile($matchingMessage->getSender());
+            $profile = Collection::make($user->getJSONDecodedBody());
+
+            $this->log->info('User profile: ',$user->getJSONDecodedBody());
+
+            $user_id = $profile->get('userId') ? $profile->get('userId') : null;
+            $display_name = $profile->get('displayName') ? $profile->get('displayName') : null;
+
+            $profile = [
+                'picture_url' => $profile->get('pictureUrl') ? $profile->get('pictureUrl') : null,
+                'status_message' => $profile->get('statusMessage') ? $profile->get('statusMessage') :null
+            ];
+
+            return new User($matchingMessage->getSender(), $display_name, null, $user_id, $profile);
         }
 
         public function getConversationAnswer(IncomingMessage $message)
