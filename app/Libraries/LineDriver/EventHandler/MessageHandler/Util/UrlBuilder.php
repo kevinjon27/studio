e5d5ee7b9e96171c943f701028b6b99dc521b5c6
Kevin Jonathan <kevin_jon27@yahoo.com>
2017-09-17 01:18:27 +0700

    url builder not using Request

--- app/Libraries/LineDriver/EventHandler/MessageHandler/Util/UrlBuilder.php
+++ app/Libraries/LineDriver/EventHandler/MessageHandler/Util/UrlBuilder.php
@@ -2,11 +2,9 @@
 
 namespace App\Libraries\LineDriver\EventHandler\MessageHandler\Util;
 
-use Symfony\Component\HttpFoundation\Request;
-
 class UrlBuilder
 {
-    public static function buildUrl(Request $req, array $paths)
+    public static function buildUrl($req, array $paths)
     {
         // NOTE: You should configure $baseUri according to your environment
         // Perhaps, it is prefer to use $_SERVER['HTTP_HOST'], $_SERVER['HTTP_X_FORWARDED_HOST'] or etc
